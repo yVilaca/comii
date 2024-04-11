62d120e62a10b9caee323907cd829723b2c5054d
Lucas <lucasvilacacast.parente@gmail.com>
2024-04-11 19:49:45 -0300

    Inicial

---
+++ src/componentes/banner/index.jsx
@@ -0,0 +1,10 @@
+import './banner.css';
+function Banner() {
+    return <div>
+        <section>
+            <h1>Seja Bem-Vindo!</h1>
+            <p>Este cardápio é 100% digital, sinta-se a vontade para navegar entre os produtos.</p>
+        </section>
+    </div>
+}
+export default Banner;
