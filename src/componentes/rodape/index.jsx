c49af63c32d37de2d25e77ea3be8832fbaa09e48
Lucas <lucasvilacacast.parente@gmail.com>
2024-05-17 22:24:44 -0300

    Correção tamanho das categorias e aba home selecionada na tela inicial

--- src/componentes/rodape/index.jsx
+++ src/componentes/rodape/index.jsx
@@ -3,38 +3,38 @@ import "./rodape.css";
 import { Outlet, Link, useLocation } from "react-router-dom";
 
 function Rodape() {
-    const location = useLocation();
-    const [activeLink, setActiveLink] = useState("");
+  const location = useLocation();
+  const [activeLink, setActiveLink] = useState("");
 
-    useEffect(() => {
-        setActiveLink(location.pathname);
-    }, [location.pathname]);
+  useEffect(() => {
+    setActiveLink(location.pathname);
+  }, [location.pathname]);
 
-    return (
-        <>
-            <footer>
-                <Link
-                    to="/home"
-                    className={`aba home ${activeLink === "/home" ? "active" : ""}`}
-                >
-                    <img src="/imgs/home.svg" alt="imagem" /> <p className="abanome">Home</p>
-                </Link>
-                <Link
-                    to="/carrinho"
-                    className={`aba cart ${activeLink === "/carrinho" ? "active" : ""}`}
-                >
-                    <img src="/imgs/shopping-cart.svg" alt="imagem" /> <p className="abanome">Carrinho</p>
-                </Link>
-                <Link
-                    to="/produtos"
-                    className={`aba products ${activeLink === "/produtos" ? "active" : ""}`}
-                >
-                    <img src="/imgs/shopping-bag.svg" alt="imagem" /><p className="abanome">Produtos</p>
-                </Link>
-            </footer>
-            <Outlet />
-        </>
-    );
+  return (
+    <>
+      <footer>
+        <Link
+          to="/home"
+          className={`aba home ${activeLink === "/home" ? "active" : ""} ${activeLink === "/" ? "active" : ""}`}
+        >
+          <img src="/imgs/home.svg" alt="imagem" /> <p className="abanome">Home</p>
+        </Link>
+        <Link
+          to="/carrinho"
+          className={`aba cart ${activeLink === "/carrinho" ? "active" : ""}`}
+        >
+          <img src="/imgs/shopping-cart.svg" alt="imagem" /> <p className="abanome">Carrinho</p>
+        </Link>
+        <Link
+          to="/produtos"
+          className={`aba products ${activeLink.startsWith("/produtos") ? "active" : ""}`}
+        >
+          <img src="/imgs/shopping-bag.svg" alt="imagem" /><p className="abanome">Produtos</p>
+        </Link>
+      </footer>
+      <Outlet />
+    </>
+  );
 }
 
 export default Rodape;
