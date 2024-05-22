31efbd5b2c0af2bd928869800adf05847fcea188
Pedro <pedrocaires13@gmail.com>
2024-05-21 21:18:26 -0300

    Primeiro Produto Bebidas

---
+++ src/componentes/item-deitado-produto/index.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useContext } from 'react';
+import './deitado-produto.css';
+import { CartContext } from '../../pages/CartContext';
+
+function ItemDBebidas(props) {
+    const { addToCart } = useContext(CartContext);
+    const [mensagem, setMensagem] = useState('');
+
+    function Adicionar(item) {
+        addToCart(item);
+        setMensagem(`${item.nome} foi incluído ao carrinho!`);
+        setTimeout(() => {
+            setMensagem('');
+        }, 3000);
+    }
+
+    return <div>
+        <section className="recomendados-text">
+            <p className="recomendados-line">------------------------------------------</p>
+            <h1 id="recomendados">BEBIDAS</h1>
+            <p className="recomendados-line">------------------------------------------</p>
+        </section>
+
+        <div className='deitado'>
+            <ul className='list-deitado'>
+                <li className="imgProduto"><img src="/imgs/agua.svg" alt="" /></li>
+                <li className='info-deitado'>
+                    <p className="nomeItem">{props.nome}</p>
+                    <p className="descItem">{props.desc}</p>
+
+                    <div id='flex-preco'>
+                        <p>R$ {props.preco}</p>
+                    </div>
+                </li>
+                <button onClick={() => Adicionar({ id: props.id, nome: props.nome, desc: props.desc, preco: props.preco })} id="mais"><img src="/imgs/mais.svg" alt="" /></button>
+            </ul>
+            {mensagem && (
+                <div className="mensagem-alerta">
+                    <img src="/imgs/check.svg" alt='check' width={"20px"} style={{ paddingRight: "1vh" }} />{mensagem}</div>
+            )}
+        </div>
+    </div>;
+}
+export default ItemDBebidas;
--- src/pages/produtos/subpages/Bebidas.js
+++ src/pages/produtos/subpages/Bebidas.js
@@ -1,29 +1,11 @@
 import React from "react";
-import './bebidas.css'
+import './bebidas.css';
+import ItemDBebidas from "../../../componentes/item-deitado-produto";
 
-function Bebidas() {
-  return <div>
-    <section className="recomendados-text">
-      <p className="recomendados-line">------------------------------------------</p>
-      <h1 id="recomendados">BEBIDAS</h1>
-      <p className="recomendados-line">------------------------------------------</p>
-    </section>
-
-    <div className='deitado'>
-      <ul className='list-deitado'>
-        <li><img src="/imgs/agua.svg" alt="" /></li>
-        <li className='imfo-deitado'>
-          <p className="nomeItem">Água</p>
-          <p className="descItem">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum sed faucibus odio. Quisque leo ligula, lobortis nec est a, mattis iaculis massa..</p>
-
-          <div id='flex-preco'>
-            <p>R$ 100,00</p>
-            <button><img src="" alt="" /></button>
-          </div>
-        </li>
-      </ul>
-    </div>
-  </div>;
+const Bebidas = () => {
+  return (
+    <ItemDBebidas nome="Água" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={9.99}/>
+  )
 }
 
 export default Bebidas;
