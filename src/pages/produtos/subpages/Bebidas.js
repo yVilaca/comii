84b3034029ba5535f08b75e46366e3a10347a19c
Pedro <pedrocaires13@gmail.com>
2024-05-23 20:51:05 -0300

    Ajuste Id Produtos

--- src/pages/produtos/subpages/Bebidas.js
+++ src/pages/produtos/subpages/Bebidas.js
@@ -8,12 +8,12 @@ function Bebidas () {
   
     <div style={{marginBottom:"8vh"}}>
       <Titulo titulo="BEBIDAS"/>
-      <ItemDBebidas id="1" nome="Água" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={9.99} img="/imgs/Água.png" />
-      <ItemDBebidas id="2" nome="Água" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={10.80} img="/imgs/Água.png" />
+      <ItemDBebidas id="7" nome="Água" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={9.99} img="/imgs/Água.png" />
+      <ItemDBebidas id="8" nome="Água" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={9.99} img="/imgs/Água.png" />
 
       <Titulo titulo="BEBIDAS ALCOÓLICAS"/>
-      <ItemDBebidas  id="2" nome="Cerveja" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={10.80} img="/imgs/cerveja.svg"/>
-      <ItemDBebidas  id="2" nome="Cerveja" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={10.80} img="/imgs/cerveja.svg"/>
+      <ItemDBebidas  id="9" nome="Cerveja" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={10.80} img="/imgs/cerveja.svg"/>
+      <ItemDBebidas  id="10" nome="Cerveja" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={10.80} img="/imgs/cerveja.svg"/>
     </div>
   )
 }
--- src/pages/produtos/subpages/Entradas.js
+++ src/pages/produtos/subpages/Entradas.js
@@ -7,10 +7,10 @@ function Entradas() {
   return(
   <div style={{marginBottom:"8vh"}}>
       <Titulo titulo="ENTRADAS"/>
-      <ItemDBebidas img="/imgs/bruschettas2.svg" id="1" nome="Bruschettas" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={29.99} />
-      <ItemDBebidas img="/imgs/carpaccio2.svg" id="1" nome="Carpaccio" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={19.99} />
-      <ItemDBebidas img="/imgs/espetinho2.svg" id="2" nome="Espetinho caprese ao molho pesto" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={14.80} />
-      <ItemDBebidas img="/imgs/cestinha2.svg" id="2" nome="Cestinha de massa de pastel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={20.80} />
+      <ItemDBebidas img="/imgs/bruschettas2.svg" id="15" nome="Bruschettas" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={29.99} />
+      <ItemDBebidas img="/imgs/carpaccio2.svg" id="16" nome="Carpaccio" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={19.99} />
+      <ItemDBebidas img="/imgs/espetinho2.svg" id="17" nome="Espetinho caprese ao molho pesto" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={14.80} />
+      <ItemDBebidas img="/imgs/cestinha2.svg" id="18" nome="Cestinha de massa de pastel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={20.80} />
     </div>
   )
 }
