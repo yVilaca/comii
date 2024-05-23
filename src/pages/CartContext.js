749107731e0c836e36b183e7cb54871e3ebf5cdd
Lucas <lucasvilacacast.parente@gmail.com>
2024-05-23 20:21:42 -0300

    Salvar carrinho nos cookies

--- src/pages/CartContext.js
+++ src/pages/CartContext.js
@@ -1,9 +1,16 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 
 export const CartContext = createContext();
 
 export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(() => {
+    const savedCart = localStorage.getItem('cart');
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart));
+  }, [cart]);
 
   const addToCart = (item) => {
     setCart((prevCart) => {
