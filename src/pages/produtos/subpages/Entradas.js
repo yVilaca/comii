112b380da23de773a714d737ade21abc82be1151
Pedro <pedrocaires13@gmail.com>
2024-05-22 21:32:32 -0300

    Todas Categorias Produtos

--- src/pages/produtos/subpages/Entradas.js
+++ src/pages/produtos/subpages/Entradas.js
@@ -1,9 +1,10 @@
 import React from "react";
-import './bebidas.css';
+import './entradas.css';
 import ItemDBebidas from "../../../componentes/item-deitado-produto";
 import Titulo from "../../../componentes/titulo";
 
 function Entradas() {
+  return(
   <div>
       <Titulo titulo="ENTRADAS"/>
       <ItemDBebidas img="/imgs/bruschettas.svg" id="1" nome="Bruschettas" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={29.99} />
@@ -11,6 +12,7 @@ function Entradas() {
       <ItemDBebidas img="/imgs/espetinho.svg" id="2" nome="Espetinho caprese ao molho pesto" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={14.80} />
       <ItemDBebidas img="/imgs/cestinha.svg" id="2" nome="Cestinha de massa de pastel" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={20.80} />
     </div>
+  )
 }
 
 export default Entradas;
--- src/pages/produtos/subpages/Sobremesas.js
+++ src/pages/produtos/subpages/Sobremesas.js
@@ -1,9 +1,10 @@
 import React from "react";
-import './bebidas.css';
+import './sobremesas.css';
 import ItemDBebidas from "../../../componentes/item-deitado-produto";
 import Titulo from "../../../componentes/titulo";
 
 function Sobremesas() {
+  return(
   <div>
       <Titulo titulo="SOBREMESAS"/>
       <ItemDBebidas img="/imgs/pudim.svg" id="1" nome="Pudim" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={29.99} />
@@ -11,6 +12,7 @@ function Sobremesas() {
       <ItemDBebidas img="/imgs/sorvete.svg" id="2" nome="Sorvete" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={14.80} />
       <ItemDBebidas img="/imgs/pave.svg" id="2" nome="Pavê" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={20.80} />
     </div>
+  )
 }
 
 export default Sobremesas;
