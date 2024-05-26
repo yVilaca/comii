0b4f83674265988b54963c474c774a8adabad23f
Lucas <lucasvilacacast.parente@gmail.com>
2024-05-25 21:30:51 -0300

    Mensagem tamanho maximo permitido

--- src/App.js
+++ src/App.js
@@ -14,22 +14,30 @@ import Entradas from './pages/produtos/subpages/Entradas';
 function App() {
   return (
     <div className="App">
-      <CartProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/carrinho" element={<Carrinho />} />
-            <Route path="/produtos" element={<Produtos />}>
-              <Route path="bebidas" element={<Bebidas />} />
-              <Route path="sobremesas" element={<Sobremesas />} />
-              <Route path="entradas" element={<Entradas />} />
-            </Route>
-          </Routes>
-          <Rodape />
-        </BrowserRouter>
-      </CartProvider>
+      <div className='conteudotodo'>
+        <CartProvider>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/carrinho" element={<Carrinho />} />
+              <Route path="/produtos" element={<Produtos />}>
+                <Route path="bebidas" element={<Bebidas />} />
+                <Route path="sobremesas" element={<Sobremesas />} />
+                <Route path="entradas" element={<Entradas />} />
+              </Route>
+            </Routes>
+            <Rodape />
+          </BrowserRouter>
+        </CartProvider>
+      </div>
+
+      <span className='msgmaxwidth' style={{ margin: "0 auto" }}>
+        PAGINA INDISPONÍVEL PARA SEU DISPOSITIVO <br/>
+        [Por favor, utilize disposivos móveis]
+      </span>
     </div>
+
   );
 }
 
