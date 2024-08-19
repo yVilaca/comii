a59538771ced10ecc1326051d7f6f007210209e7
Lucas <lucasvilacacast.parente@gmail.com>
2024-08-19 20:07:21 -0300

    toretto commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
-import './app.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { CartProvider } from './pages/CartContext';
+import React from "react";
+import "./app.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { CartProvider } from "./pages/CartContext";
 
-import Home from './pages/Home';
-import Carrinho from './pages/Carrinho';
-import Produtos from './pages/Produtos';
-import Bebidas from './pages/produtos/subpages/Bebidas';
-import Sobremesas from './pages/produtos/subpages/Sobremesas';
-import Entradas from './pages/produtos/subpages/Entradas';
-import Orders from './pages/Orders'; // Importe o componente Orders aqui
-import Rodape from './componentes/rodape';
-import Ajuda from './pages/Ajuda';
+import Home from "./pages/Home";
+import Carrinho from "./pages/Carrinho";
+import Produtos from "./pages/Produtos";
+import Bebidas from "./pages/produtos/subpages/Bebidas";
+import Sobremesas from "./pages/produtos/subpages/Sobremesas";
+import Entradas from "./pages/produtos/subpages/Entradas";
+import Orders from "./pages/Orders"; // Importe o componente Orders aqui
+import Rodape from "./componentes/rodape";
+import Ajuda from "./pages/Ajuda";
 
 function App() {
   return (
     <div className="App">
-      <div className='conteudotodo'>
+      <div className="conteudotodo">
         <CartProvider>
           <BrowserRouter>
             <Routes>
@@ -35,8 +35,8 @@ function App() {
           </BrowserRouter>
         </CartProvider>
       </div>
-
-      <span className='msgmaxwidth' style={{ margin: "0 auto" }}>
+      {/* teste */}
+      <span className="msgmaxwidth" style={{ margin: "0 auto" }}>
         PAGINA INDISPONÍVEL PARA SEU DISPOSITIVO <br />
         [Por favor, utilize dispositivos móveis]
       </span>
