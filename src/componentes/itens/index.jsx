48f8532bbf0b622335cbae9c044c64550162f969
Lucas <lucasvilacacast.parente@gmail.com>
2024-09-01 09:41:39 -0300

    scroll home

--- src/componentes/itens/index.jsx
+++ src/componentes/itens/index.jsx
@@ -1,54 +1,65 @@
 // Itens.jsx
-import React, { useState, useContext } from 'react';
-import './itens.css';
-import { CartContext } from '../../pages/CartContext';
+import React, { useState, useContext } from "react";
+import "./itens.css";
+import { CartContext } from "../../pages/CartContext";
 
 function Itens(props) {
-    const [mensagem, setMensagem] = useState('');
-    const { addToCart, cart } = useContext(CartContext);
+  const [mensagem, setMensagem] = useState("");
+  const { addToCart, cart } = useContext(CartContext);
 
-    function Adicionar() {
-        const itemExistenteIndex = cart.findIndex((cartItem) => cartItem.id === props.id);
+  function Adicionar() {
+    const itemExistenteIndex = cart.findIndex(
+      (cartItem) => cartItem.id === props.id
+    );
 
-        if (itemExistenteIndex !== -1) {
-            // Se o item já existe, apenas atualiza a quantidade
-            const updatedCart = [...cart];
-            updatedCart[itemExistenteIndex].quantidade += 1;
-            addToCart(updatedCart);
-        } else {
-            // Se não existe, adiciona ao carrinho
-            const itemToAdd = {
-                id: props.id,
-                nome: props.nome,
-                desc: props.desc,
-                preco: props.preco,
-                quantidade: 1,
-                img: props.img
-            };
-            addToCart(itemToAdd);
-        }
-
-        setMensagem(`${props.nome} foi incluído ao carrinho!`);
-        setTimeout(() => {
-            setMensagem('');
-        }, 3000);
+    if (itemExistenteIndex !== -1) {
+      // Se o item já existe, apenas atualiza a quantidade
+      const updatedCart = [...cart];
+      updatedCart[itemExistenteIndex].quantidade += 1;
+      addToCart(updatedCart);
+    } else {
+      // Se não existe, adiciona ao carrinho
+      const itemToAdd = {
+        id: props.id,
+        nome: props.nome,
+        desc: props.desc,
+        preco: props.preco,
+        quantidade: 1,
+        img: props.img,
+      };
+      addToCart(itemToAdd);
     }
 
-    return (
-        <div>
-            <ul id='item-geral'>
-                <img src={props.img} alt={props.nome} />
-                <li id='linha-1'><p>{props.nome}</p></li>
-                <li id='desc-item'>{props.desc}</li>
-                <li id='preco'>R$ {props.preco.toFixed(2)}</li>
-                <button onClick={Adicionar}>Incluir ao carrinho</button>
-            </ul>
-            {mensagem && (
-                <div className="mensagem-alerta">
-                    <img src="/imgs/check.svg" alt='check' width={"20px"} style={{ paddingRight: "1vh" }} />{mensagem}</div>
-            )}
+    setMensagem(`${props.nome} foi incluído ao carrinho!`);
+    setTimeout(() => {
+      setMensagem("");
+    }, 3000);
+  }
+
+  return (
+    <div>
+      <ul id="item-geral">
+        <img src={props.img} alt={props.nome} />
+        <li id="linha-1">
+          <p>{props.nome}</p>
+        </li>
+        <li id="desc-item">{props.desc}</li>
+        <li id="preco">R$ {props.preco.toFixed(2)}</li>
+        <button onClick={Adicionar}>Inncluir ao carrinho</button>
+      </ul>
+      {mensagem && (
+        <div className="mensagem-alerta">
+          <img
+            src="/imgs/check.svg"
+            alt="check"
+            width={"20px"}
+            style={{ paddingRight: "1vh" }}
+          />
+          {mensagem}
         </div>
-    );
+      )}
+    </div>
+  );
 }
 
 export default Itens;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,50 +1,104 @@
-import React from 'react';
-import Banner from '../componentes/banner';
-import ItemD from '../componentes/item-deitado';
-import Itens from '../componentes/itens';
-import NavBar from '../componentes/topbar';
-import './home.css';
+import React from "react";
+import Banner from "../componentes/banner";
+import ItemD from "../componentes/item-deitado";
+import Itens from "../componentes/itens";
+import NavBar from "../componentes/topbar";
+import "./home.css";
 
 const Home = () => {
-    const items = [
-        { id: 13, nome: "Cheese Burguer", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", preco: 19.80 },
-        { id: 14, nome: "X-Bacon", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", preco: 13.99 },
-        { id: 15, nome: "X-Tudo", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", preco: 17.50 },
-        { id: 16, nome: "X-Vegetariano", desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.", preco: 15.00 }
-    ];
-    const categoriasJuntasItems = [
-        { id: 17, nome: "Burrito", preco: 19.80, desc: "Burrito do chefe com queijos especiais e temperos italianos selecionados." },
-        { id: 18, nome: "Macarrão", preco: 13.99, desc: "Macarrão com queijos especiais e temperos italianos selecionados." } 
-    ];
+  const items = [
+    {
+      id: 13,
+      nome: "Cheese Burguer",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      preco: 19.8,
+    },
+    {
+      id: 14,
+      nome: "X-Bacon",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      preco: 13.99,
+    },
+    {
+      id: 15,
+      nome: "X-Tudo",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      preco: 17.5,
+    },
+    {
+      id: 16,
+      nome: "X-Vegetariano",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      preco: 15.0,
+    },
+  ];
+  const categoriasJuntasItems = [
+    {
+      id: 17,
+      nome: "Burrito",
+      preco: 19.8,
+      desc: "Burrito do chefe com queijos especiais e temperos italianos selecionados.",
+    },
+    {
+      id: 18,
+      nome: "Macarrão",
+      preco: 13.99,
+      desc: "Macarrão com queijos especiais e temperos italianos selecionados.",
+    },
+    {
+      id: 19,
+      nome: "Fritas",
+      preco: 25.8,
+      desc: "Deliciosa porção de batatas fritas palito com sal.",
+    },
+    {
+      id: 20,
+      nome: "Espetinho",
+      preco: 7.5,
+      desc: "Maça de peito em cubinhos assada na grelha e tempero do chefe.",
+    },
+  ];
 
-    return (
-        <div>
-            <NavBar />
-            <Banner />
+  return (
+    <div>
+      <NavBar />
+      <Banner />
 
-            <section className="recomendados-text">
-                <p className="recomendados-line">-----------------------------------</p>
-                <h1 id="recomendados">RECOMENDADOS</h1>
-                <p className="recomendados-line">-----------------------------------</p>
-            </section>
+      <section className="recomendados-text">
+        <p className="recomendados-line">-----------------------------------</p>
+        <h1 id="recomendados">RECOMENDADOS</h1>
+        <p className="recomendados-line">-----------------------------------</p>
+      </section>
 
-            <div className='categorias-juntas2'>
-                {categoriasJuntasItems.map(item => (
-                    <a href="#item" key={item.id}>
-                        <Itens id={item.id} nome={item.nome} preco={item.preco} desc={item.desc} img={"/imgs/" + item.nome + ".png"} />
-                    </a>
-                ))}
-            </div>
+      <div className="categorias-juntas2">
+        {categoriasJuntasItems.map((item) => (
+          <div key={item.id} className="item-container">
+            <Itens
+              id={item.id}
+              nome={item.nome}
+              preco={item.preco}
+              desc={item.desc}
+              img={"/imgs/" + item.nome + ".png"}
+            />
+          </div>
+        ))}
+      </div>
 
-            <div className="itens" style={{ marginBottom: "15%" }}>
-                {items.map(item => (
-                    <div key={item.id} style={{ marginBottom: '15px' }}>
-                        <ItemD id={item.id} nome={item.nome} desc={item.desc} preco={item.preco} img={"/imgs/" + item.nome + ".png"} />
-                    </div>
-                ))}
-            </div>
-        </div>
-    );
+      <div className="itens" style={{ marginBottom: "15%" }}>
+        {items.map((item) => (
+          <div key={item.id} style={{ marginBottom: "15px" }}>
+            <ItemD
+              id={item.id}
+              nome={item.nome}
+              desc={item.desc}
+              preco={item.preco}
+              img={"/imgs/" + item.nome + ".png"}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
