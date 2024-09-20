9f07f4449e1980a8f79fbd54dd552d3b9fcfdf3c
Pedro <pedrocaires13@gmail.com>
2024-09-20 17:56:29 -0300

    imgs bebidas

--- src/pages/produtos/subpages/Bebidas.js
+++ src/pages/produtos/subpages/Bebidas.js
@@ -9,11 +9,11 @@ function Bebidas() {
     <div style={{ marginBottom: "8vh" }}>
       <Titulo titulo="BEBIDAS" linha="-------------------------------------------" />
       <ItemDBebidas id="1" nome="Água sem gás" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={3.00} img="/imgs/Água.png" />
-      <ItemDBebidas id="2" nome="Água com gás" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={3.99} img="/imgs/Água.png" />
+      <ItemDBebidas id="2" nome="Água com gás" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={3.99} img="/imgs/agua_gasosa.jpg" />
 
       <Titulo titulo="BEBIDAS ALCOÓLICAS" />
-      <ItemDBebidas id="3" nome="Heineken" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={10.80} img="/imgs/cerveja.svg" />
-      <ItemDBebidas id="4" nome="Kaiser" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={7.80} img="/imgs/cerveja.svg" />
+      <ItemDBebidas id="3" nome="Heineken" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={10.80} img="/imgs/heinneken.jpg" />
+      <ItemDBebidas id="4" nome="Corona" desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit." preco={7.80} img="/imgs/corona.png" />
     </div>
   )
 }
