db2a57ed6bd615f0894a13f9c143e5ed17a37cd6
Pedro <pedrocaires13@gmail.com>
2024-09-20 17:22:37 -0300

    Notificacao garcom solicitado

--- src/componentes/chamar-gançom/index.jsx
+++ src/componentes/chamar-gançom/index.jsx
@@ -1,14 +1,31 @@
+import React from "react";
 import "./chamar.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-function ChamarGarcom(){
+function ChamarGarcom() {
+  const chamarGarcom = () => {
+    toast.info("O garçom foi solicitado para sua mesa.", {
+      position: "top-right",
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      draggablePercent: 40,
+    });
+    
+    // Aqui você pode adicionar lógica adicional, como enviar uma solicitação para o servidor
+  };
 
-    return(
-        <div>
-            <div id="chamar">
-                Chamar Garçom
-            </div>
-        </div>
-    )
-
+  return (
+    <div>
+      <div id="chamar" onClick={chamarGarcom}>
+        Chamar Garçom
+      </div>
+    </div>
+  );
 }
+
 export default ChamarGarcom;
