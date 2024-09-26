f3bc09d0cfb81eb4bff5ace48d32ea7b79e19cf1
Pedro <pedrocaires13@gmail.com>
2024-09-26 20:49:17 -0300

    Teste Carrossel Auto

--- src/componentes/carrossel/index.jsx
+++ src/componentes/carrossel/index.jsx
@@ -1,13 +1,19 @@
+import React from 'react';
 import './carrossel.css';
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, EffectFade } from 'swiper/modules';
+
+// Importe os estilos necessários do Swiper
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-fade';
 
 function Carrossel() {
-
     const data = [
-        { id: "1", image: <img src="./597575.jpg" alt="" /> },
-        { id: "2", image: <img src="./597575.jpg" alt="" /> },
-        { id: "3", image: <img src="./597575.jpg" alt="" /> },
-    ]
+        { id: "1", image: "/imgs/597575.jpg" },
+        { id: "2", image: "/imgs/597575.jpg" },
+        { id: "3", image: "/imgs/597575.jpg" },
+    ];
 
     return (
         <div>
@@ -15,17 +21,25 @@ function Carrossel() {
             <div className='container'>
                 <Swiper
                     slidesPerView={1}
-                    pagination={{ clickable: true }}>
+                    effect={'fade'}
+                    loop={true}
+                    autoplay={{
+                        delay: 5000,
+                        disableOnInteraction: false,
+                    }}
+                    pagination={{ clickable: true }}
+                    modules={[Autoplay, Pagination, EffectFade]}
+                    className="mySwiper"
+                >
                     {data.map((item) => (
                         <SwiperSlide key={item.id} className='slide-img'>
-                            <img src="/imgs/597575.jpg" alt='' className='slide-item' />
-                            <img src="/imgs/597575.jpg" alt='' className='slide-item item2' />
-                            <img src="/imgs/597575.jpg" alt='' className='slide-item' />
+                            <img src={item.image} alt='' className='slide-item' />
                         </SwiperSlide>
                     ))}
                 </Swiper>
             </div>
         </div>
-    )
+    );
 }
+
 export default Carrossel;
